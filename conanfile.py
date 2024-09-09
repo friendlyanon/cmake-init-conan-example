70b0c6a6879208e9302b0affedfd54468f67cc19
friendlyanon <friendlyanon@users.noreply.github.com>
2024-09-09 21:39:03 +0200

    Update to v0.40.8

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class Recipe(ConanFile):
         self.folders.generators = "conan"
 
     def requirements(self):
-        self.requires("fmt/10.2.1")
+        self.requires("fmt/11.0.2")
 
     def build_requirements(self):
-        self.test_requires("catch2/3.6.0")
+        self.test_requires("catch2/3.7.0")
